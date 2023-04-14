d9cb0a81df4d80609a8a001e1699aada59f95d51
Nasser Mughrabi <abdalnasseralmughrabi@gmail.com>
2023-04-13 21:52:30 -0600

    added more info to profile and fixed css styling

--- frontend/src/components/ListFeed.js
+++ frontend/src/components/ListFeed.js
@@ -27,7 +27,7 @@ const ListFeed = () => {
             <article className='post-article'>
               <div class='container mt-4 mb-5'>
                 <div class='d-flex justify-content-center row'>
-                  <div class='col-md-8'>
+                  <div id="col-md-8-id" class='col-md-8'>
                     <div class='feed p-2'>
                       <div class='bg-white border mt-2'>
                         <div>
